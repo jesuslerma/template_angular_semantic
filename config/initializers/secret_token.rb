af6a752a8d820fcd726ee9edb11a1670737b4f20
Jesus Lerma <shuyojl@gmail.com>
2015-01-27 23:57:34 -0600

    first init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TemplateAngular::Application.config.secret_token = '30b7665d5281801ba6652f22fefd89ca16c27f2c435e447f41cc11763f6114099f79121ac2df39b8dbe7f94c57f5cd4830043d8159c35c0ec02c4dba0d5ca6bc'
